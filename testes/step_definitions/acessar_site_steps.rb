f7851ccc2bc5ff843e98be975efb729578368759
IrPedro <irpedrogsp@usp.br>
2024-12-17 20:54:24 -0300

    a

--- testes/step_definitions/acessar_site_steps.rb
+++
@@ -1,7 +0,0 @@
-Given('que eu acesso o site {string}') do |url|
-  visit url
-end
-
-Then('eu devo ver {string}') do |text|
-  expect(page).to have_content(text)
-end
--- testes/step_definitions/pesquisar_cidade_steps.rb
+++
@@ -1,3 +0,0 @@
-When('eu preencho o campo {string} com {string}') do |field, value|
-  fill_in field, with: value
-end
